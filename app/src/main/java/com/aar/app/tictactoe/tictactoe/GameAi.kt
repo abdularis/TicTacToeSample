3c7671fda49b8b7e20f8926818a3dca94be4c424
Abdul Aris <abdularisrahmanudin10@gmail.com>
2018-06-30 06:24:18 +0700

    fixed issue in alpha beta pruning

--- app/src/main/java/com/aar/app/tictactoe/tictactoe/GameAi.kt
+++ app/src/main/java/com/aar/app/tictactoe/tictactoe/GameAi.kt
@@ -7,33 +7,33 @@ class GameAi(val maxPlayer: Int, val minPlayer: Int) {
         private const val VAL_WORST_MIN = BoardEvaluator.VAL_MAX * 10
     }
 
-    fun findBestMove(board: Board, player: Int): Move = minimax(board, player)
+    fun findBestMove(board: Board, player: Int) = minimax(board, player, BoardEvaluator.VAL_MIN, BoardEvaluator.VAL_MAX)
 
-    private fun minimax(board: Board, player: Int): Move {
+    private fun minimax(board: Board, player: Int, alpha: Int, beta: Int): Move {
         val score = BoardEvaluator.evaluate(board, maxPlayer, minPlayer)
         if (board.isFull || score == BoardEvaluator.VAL_MAX || score == BoardEvaluator.VAL_MIN) {
             return Move(score = score)
         }
 
-        var alpha = BoardEvaluator.VAL_MIN
-        var beta = BoardEvaluator.VAL_MAX
+        var currAlpha = alpha
+        var currBeta = beta
         var bestMove = Move(score = if (player == maxPlayer) VAL_WORST_MAX else VAL_WORST_MIN)
         for (i in 0 until board.data.size) {
-            if (board[i] != 0) continue
+            if (board[i] != EMPTY_TOKEN) continue
 
             board[i] = player
-            val move = minimax(board, if (player == maxPlayer) minPlayer else maxPlayer)
+            val move = minimax(board, if (player == maxPlayer) minPlayer else maxPlayer, currAlpha, currBeta)
             board[i] = EMPTY_TOKEN
 
             if (player == maxPlayer && move.score > bestMove.score) {
                 bestMove = Move(i, move.score)
-                alpha = Math.max(alpha, bestMove.score)
+                currAlpha = Math.max(currAlpha, bestMove.score)
             } else if (player == minPlayer && move.score < bestMove.score) {
                 bestMove = Move(i, move.score)
-                beta = Math.min(beta, bestMove.score)
+                currBeta = Math.min(currBeta, bestMove.score)
             }
 
-            if (beta <= alpha)
+            if (currBeta <= currAlpha)
                 break
         }
 
